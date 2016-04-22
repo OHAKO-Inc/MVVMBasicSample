7fe11b4c4d2db96a713f5893024e638d7f769a99
Yoshikuni Kato <y.kato@ohako-inc.jp>
2016-04-22 16:24:33 +0900

    Initial Commit

---
+++ MVVMBasicSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMBasicSample
+//
+//  Created by Yoshikuni Kato on 2016/04/22.
+//  Copyright © 2016年 Ohako Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
