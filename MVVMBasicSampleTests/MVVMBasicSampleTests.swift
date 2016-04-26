056bf5a3f8055179a38e7da00b24d9161f12dcfe
Yoshikuni Kato <y.kato@ohako-inc.jp>
2016-04-26 13:36:56 +0900

    Add basic tests

--- MVVMBasicSampleTests/MVVMBasicSampleTests.swift
+++ MVVMBasicSampleTests/MVVMBasicSampleTests.swift
@@ -7,29 +7,92 @@
 //
 
 import XCTest
+import ReactiveCocoa
+import Result
 @testable import MVVMBasicSample
 
 class MVVMBasicSampleTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testViewWithoutMutableProperty() {
+        let user = User(firstName: "Yoshikuni", lastName: "Kato", age: 27, imageName: "yoshikuni")
+        let expectedFullName = "Yoshikuni Kato"
+        
+        let userNameViewModel = UserNameViewModel(user: user)
+        XCTAssertEqual(userNameViewModel.fullName, expectedFullName)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testTableViewWithoutMutableProperty() {
+        let users = [
+            User(firstName: "Emma", lastName: "Bradley", age: 27, imageName: "yoshikuni"),
+            User(firstName: "Alice", lastName: "Baker", age: 51, imageName: "yoshikuni"),
+            User(firstName: "Edward", lastName: "Diaz", age: 10, imageName: "yoshikuni")
+        ]
+        let expectedFullnames = [
+            "Emma Bradley",
+            "Alice Baker",
+            "Edward Diaz"
+        ]
+        
+        let cellModels: [UsersTableCellModelType] = users.map { (user) -> UsersTableCellModel in
+            return UsersTableCellModel(user: user)
+        }
+        let usersViewModel = UsersViewModel(cellModels: cellModels)
+
+        for (index, cellModel) in usersViewModel.cellModels.enumerate() {
+            XCTAssertEqual(cellModel.fullname, expectedFullnames[index])
+        }
+    }
+    
+    func testViewWithMutablePropertyUsingClosure() {
+
+        // for asynchronous test
+        let expectation = self.expectationWithDescription("image set")
+
+        let user = User(firstName: "Yoshikuni", lastName: "Kato", age: 27, imageName: "yoshikuni")
+        let userViewModel = UserViewModel(user: user)
+        userViewModel.imageDidSetClosure = { (image) in
+            XCTAssertNotNil(image, "image not nil")
+            expectation.fulfill()
+        }
+        userViewModel.imageFetchButtonTapped()
+
+        waitForExpectationsWithTimeout(1.5) { (error) in
+            print(error)
+        }
+        
+    }
+    
+    func testViewWithMutablePropertyUsingRAC() {
+        // for asynchronous test
+        let expectation = self.expectationWithDescription("image set")
+        
+        let user = User(firstName: "Yoshikuni", lastName: "Kato", age: 27, imageName: "yoshikuni")
+        let userViewModel = UserViewModelWithRAC(user: user)
+        userViewModel
+            .image
+            .signal
+            .observeNext { (image) in
+                XCTAssertNotNil(image, "image not nil")
+                expectation.fulfill()
+        }
+        
+        userViewModel
+            .imageFetchButtonAction
+            .apply(nil)
+            .start()
+        
+        waitForExpectationsWithTimeout(1.5) { (error) in
+            print(error)
         }
     }
     
